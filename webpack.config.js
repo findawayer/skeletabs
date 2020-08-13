070081e2823620d7c4ec633b77e216344bfd1697
findawayer <findawayer@gmail.com>
2020-08-13 17:37:09 +0900

    use yarn, webpack4 and eslint/prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,95 @@
-var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// Node.js path helper
+const path = require('path');
+// allow access to built-in plugins
+const webpack = require('webpack');
+// CSS extractor
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// custom JS optimizer
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// project metadata
+const pkg = require('./package.json');
 
-module.exports = {
-    context: __dirname,
-    entry: __dirname + "/src/js/skeletabs.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "skeletabs.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: "style!css"
+// webpack configurations
+let config = {
+  mode: 'production',
+  entry: path.join(__dirname, pkg.main),
+  output: {
+    // use the entry chunk name as the file name
+    filename: `${pkg.name}.js`,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${pkg.name}.css`,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // inject css into DOM as <style /> node
+          // 'style-loader',
+          // extract as separate CSS file
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
             },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [{
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: "expanded",
-                            indentWidth: 4
-                        }
-                    }]
-                })
+          },
+          // translate CSS into CommonJS
+          'css-loader',
+          // process with postcss (including autoprefix)
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')],
             },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                query: {
-                    plugins: [
-                        "transform-es2015-classes",
-                        "transform-es3-member-expression-literals",
-                        "transform-es3-property-literals"
-                    ],
-                    presets: ["es2015"]
-                }
-            }
-        ]
-    },
-    plugins: [
-        //new webpack.optimize.UglifyJsPlugin(),
-        new ExtractTextPlugin("skeletabs.css")
-    ]
+          },
+          // compil sass to css
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: true,
+          mangle: false,
+          ie8: false,
+        },
+      }),
+    ],
+  },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  config = Object.assign(config, {
+    mode: 'development',
+    devServer: {
+      contentBase: __dirname,
+      hot: true,
+      open: {
+        app: ['Chrome', '--incognito'],
+      },
+    },
+    // source mapping
+    devtool: 'eval-cheap-module-source-map',
+    // logger
+    stats: 'minimal',
+  });
+}
+
+module.exports = config;
